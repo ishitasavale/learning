a362bb82eeb1ed5dc484e9a95c89e1c8fd807c27
Ishita Savale <ishitasavale1@gmail.com>
2025-02-26 14:34:32 +0530

    adde input learning file

---
+++ com/ishita/Input.java
@@ -0,0 +1,10 @@
+package com.ishita;
+
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int rollno = input.nextInt();
+        System.out.println("Your roll no is" + rollno );
+    }
+}
--- com/ishita/Main.java
+++ com/ishita/Main.java
@@ -2,6 +2,6 @@ package com.ishita;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Hello world");
     }
 }
