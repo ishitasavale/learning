70b081c7c9fd66bc903d2b12081c71e7d7d5ea14
Ishita Savale <ishitasavale1@gmail.com>
2025-02-28 11:57:53 +0530

    Loops and fibonacci

---
+++ com/ishita/Fibonacci.java
@@ -0,0 +1,26 @@
+package com.ishita;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+//1 1 2 3 5 8 13..........
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the nth number of fibonacci series you want to find :");
+        int n = in.nextInt();
+
+        int p = 0;
+        int ans = 1;
+        int count = 2;
+
+        while(count <= n){
+            int temp = ans;
+            ans = ans + p;
+            p = temp;
+            count++;
+        }
+        System.out.println("So the" + n + "th fibonacci series number is = " + ans);
+
+    }
+}
--- com/ishita/Input.java
+++ com/ishita/Input.java
@@ -3,8 +3,18 @@ package com.ishita;
 import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int rollno = input.nextInt();
-        System.out.println("Your roll no is" + rollno );
+        Scanner in = new Scanner(System.in);
+//        int rollno = input.nextInt();
+//        System.out.println("Your roll no is" + rollno );
+//    }
+          char ch = in.next().trim().charAt(0);
+//        System.out.println(ch.charAt(3));
+
+          if(ch >= 'a' && ch <= 'z'){
+              System.out.println("Lowercase");
+          }
+          else{
+              System.out.println("Uppercase");
+          }
     }
 }
